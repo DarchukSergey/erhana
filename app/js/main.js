16e41d0ff2d9456167bbc61e2c2576361e3ff668
Darchuk Sergey <Darchuk.Sergey@gmail.com>
2023-03-24 01:25:40 +0200

    added stye prefinal

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     $('.result__slider').slick({
+      infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false
     });
   });
 
   $(document).ready(function(){
     $('.roof__slider').slick({
+      infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false
     });
   });
