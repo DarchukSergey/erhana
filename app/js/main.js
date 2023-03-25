021661548ecf565b488d4660f60f740e9bdce729
Sergey Darchuk <darchuk.sergey@gmail.com>
2023-03-25 10:07:51 +0200

    added style final not update

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,25 @@
-$(document).ready(function(){
-    $('.result__slider').slick({
-      infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: false
-    });
+$(document).ready(function () {
+  $('.result__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    
+    centerPadding: '150px',
+    dotsClass: 'result__slider-dots'
   });
+});
 
-  $(document).ready(function(){
-    $('.roof__slider').slick({
-      infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: false
-    });
+$(document).ready(function () {
+  $('.roof__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    centerMode: true,
+    centerPadding: '10px',
+    dotsClass: 'proof__slider-dots'
   });
+});
